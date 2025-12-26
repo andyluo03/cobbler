140c33474640f014a45c41fdb7e9cd9b5690fcaf
Andy <andyluo918@gmail.com>
2025-12-26 15:10:35 -0600

    Load Model into GPU

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
+from dacite import from_dict
 from typing import Optional
 from safetensors import safe_open
 import torch
 import json
-
+from transformers import AutoTokenizer
 
 @dataclass
 class Qwen3Config:
@@ -32,33 +33,111 @@ class Qwen3Config:
   use_sliding_window: bool
   vocab_size: int
 
-# TODO (andyluo03): support multiple Qwen3 models.
-class Qwen3(torch.nn.Module):
-    def __init__(self, folder):
-        with safe_open(f'{folder}/model-00001-of-00003.safetensors', framework="pt", device="cuda:0") as f:
-            for key in f.keys():
-                shape = f.get_slice(key).get_shape()
-                print(f"{key}: {shape}")
-        
-        with safe_open(f'{folder}/model-00002-of-00003.safetensors', framework="pt") as f:
-            for key in f.keys():
-                shape = f.get_slice(key).get_shape()
-                print(f"{key}: {shape}")
 
-        with safe_open(f'{folder}/model-00003-of-00003.safetensors', framework="pt") as f:
-            for key in f.keys():
-                shape = f.get_slice(key).get_shape()
-                print(f"{key}: {shape}")
-
+@dataclass
+class Qwen3Layer:
+    input_layernorm: torch.Tensor
+    mlp_down_proj: torch.Tensor
+    mlp_gate_proj: torch.Tensor
+    mlp_up_proj: torch.Tensor
+    post_attention_layernorm: torch.Tensor
+    self_attn_k_norm: torch.Tensor
+    self_attn_k_proj: torch.Tensor
+    self_attn_o_proj: torch.Tensor
+    self_attn_q_norm: torch.Tensor
+    self_attn_q_proj: torch.Tensor
+    self_attn_v_proj: torch.Tensor
+    
+    # TODO (andyluo03): implement.
+    def forwards(self, inp: torch.Tensor, out: torch.Tensor):
+        # [Sequence, Embedding Size] --> [Sequence, Embedding Size]
         pass
 
-    def forward(input, batch_size=1) -> list[str]:
+# TODO (andyluo03): support multiple Qwen3 models.
+class Qwen3:
+    def __init__(self, folder):
+        # 0. Load Metadata
+        with open(f'{folder}/config.json') as f:
+            config = json.load(f)
+            self.config = from_dict(data_class=Qwen3Config, data=config)
+
+        with open(f'{folder}/model.safetensors.index.json') as f:
+            tensor_index = json.load(f)
+
+        # 1. Load Tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(folder, use_fast=True)
+        
+        # 2. Load Embedding And Norm Weights
+        def load_weight(name):
+            with safe_open(f'{folder}/{tensor_index['weight_map'][name]}', framework="pt", device="cuda:0") as f:
+                return f.get_tensor(name) # tensor persists, slice lazy loads
+
+        self.embed_tokens = load_weight('model.embed_tokens.weight')
+        self.norm = load_weight('model.norm.weight')
+        
+        # 3. Load Hidden Layer Weights
+        self.hidden_layers = []
+
+        hidden_weights = [
+            "input_layernorm",
+            "mlp.down_proj",
+            "mlp.gate_proj",
+            "mlp.up_proj",
+            "post_attention_layernorm",
+            "self_attn.k_norm",
+            "self_attn.k_proj",
+            "self_attn.o_proj",
+            "self_attn.q_norm",
+            "self_attn.q_proj",
+            "self_attn.v_proj"
+        ]
+
+        for i in range(self.config.num_hidden_layers):
+            prefix = f'model.layers.{i}.'
+
+            layer_data = {
+                "input_layernorm":          load_weight(f'{prefix}input_layernorm.weight'),
+                "mlp_down_proj":            load_weight(f'{prefix}mlp.down_proj.weight'),
+                "mlp_gate_proj":            load_weight(f'{prefix}mlp.gate_proj.weight'),
+                "mlp_up_proj":              load_weight(f'{prefix}mlp.up_proj.weight'),
+                "post_attention_layernorm": load_weight(f'{prefix}post_attention_layernorm.weight'),
+                "self_attn_k_norm":         load_weight(f'{prefix}self_attn.k_norm.weight'),
+                "self_attn_k_proj":         load_weight(f'{prefix}self_attn.k_proj.weight'),
+                "self_attn_o_proj":         load_weight(f'{prefix}self_attn.o_proj.weight'),
+                "self_attn_q_norm":         load_weight(f'{prefix}self_attn.q_norm.weight'),
+                "self_attn_q_proj":         load_weight(f'{prefix}self_attn.q_proj.weight'),
+                "self_attn_v_proj":         load_weight(f'{prefix}self_attn.v_proj.weight'),
+            }
+
+            self.hidden_layers.append(Qwen3Layer(**layer_data))
+
+    def forward(self, input: list[int], batch_size=1) -> list[int]:
+        input = torch.Tensor()
+        output = torch.Tensor()
+
+        # 0. Embed
+
+        # 1. Run Hidden Layers
+        for hidden_layer in range(self.hidden_layers):
+            hidden_layer.forward(input, output)
+            input, output = output, input
+
+        # 2. Generate Distribution
+
+    def print_summary(self):
         pass
 
 if __name__ == '__main__':
-    # Load Model
+    # 0. Load Model
     model = Qwen3('Qwen3-4B')
 
-    # Run Forwards
-    output = model.forward()
-    print(output[0])
+    # 1. Run Model
+    prompt = 'The meaning of life is'
+    tokens = model.tokenizer(prompt)['input_ids'] 
+
+    while True: 
+        # output_distribution = model.forward(input=tokens)
+        # TODO (andyluo03): sample from distribution + append to tokens
+        pass
+
+    print(final_output)
